32b97d86f15c53df45ce702b286f14d00e070dce
Chris Adams <adamscn12@gmail.com>
2022-04-01 14:30:59 -0400

    Commit with remote repo

---
+++ PlantWatering.py
@@ -0,0 +1,38 @@
+Class Plant:
+    def _init_(plant, name, amtWater):
+        plant.name = name
+        plant.amtWater = amtWater
+
+
+Class LinkedList:
+    Class Node:
+        def _init_(self, value, next):
+            self.value = value
+            self.next = next
+           
+        def _init_(self, value):
+        	self.value = value
+        	self.next = null
+    
+    def isEmpty()
+     	if(first == null):
+     		return true
+     	else:
+     		return false
+     		
+    def addNode(value):
+  	 	if(isEmpty()):
+     		first = Node(value)
+     		last = first
+     	else:
+     		last.next = Node(value)
+     		last = last.next
+    
+    def removeNode(value):
+    	if(isEmpty()):
+    		print("No Plants Entered")
+    	else:
+    		
+    		
+    	
+     		
